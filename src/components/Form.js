e85cd91d954cf945a486aa3299b9481054ef3b55
ClarisNjoga <mimonjoga@gmail.com>
2023-01-19 23:11:57 +0300

    assignment completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+function Form() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,13 +13,95 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
 export default Form;
+The above component will render previous form submissions on the page! We have a fully functioning controlled form.
+
+Validating Inputs
+One benefit we get from having our form's input values held in state is an easy way to perform validations when the form is submitted. For example, let's say we want to require that a user enter some data into our form fields before they can submit the form successfully.
+
+In our handleSubmit function, we can add some validation logic to check if the form inputs have the required data, and hold some error messages in state:
+
+// add state for holding error messages
+const [errors, setErrors] = useState([]);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // first name is required
+  if (firstName.length > 0) {
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
+  }
+}
+
+const [errors, setErrors] = useState([]);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // first name is required
+  if (firstName.length > 0) {
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
+  }
+}
+
+return (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <button type="submit">Submit</button>
+    </form>
+    {/* conditionally render error messages */}
+    {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+  </div>
+);
